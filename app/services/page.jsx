5ab6b9a0d805d765bf7694c05e47b05f6b2b6906
Md Firoj Kabir <firoj.kabir65@gmail.com>
2024-10-01 11:51:18 +0200

    Develop services page

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,5 +1,85 @@
+"use client";
+
+import Link from "next/link";
+import { BsArrowDownRight } from "react-icons/bs";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellendus facere facilis fuga mollitia.",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "UI Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellendus facere facilis fuga mollitia.",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "Testing",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellendus facere facilis fuga mollitia.",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores repellendus facere facilis fuga mollitia.",
+    href: "",
+  },
+];
+
+import { motion } from "framer-motion";
+
 const Services = () => {
-  return <div>services page</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((service, index) => {
+            return (
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {service.num}
+                  </div>
+                  <Link
+                    href={service.href}
+                    className="transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="group-hover:text-accent text-3xl" />
+                  </Link>
+                </div>
+                {/* title */}
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {service.title}
+                </h2>
+                {/* description */}
+                <p className="text-white/60">{service.description}</p>
+                {/* border */}
+                <div className="border-b border-white/20 w-full"></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
